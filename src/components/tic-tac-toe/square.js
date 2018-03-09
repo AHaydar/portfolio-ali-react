545f509173fab6c37895c6604e1c8d2031b110a4
Ali Haydar <a.haydar85@gmail.com>
2018-03-10 08:54:07 +1300

    Finalize basic game

--- src/components/tic-tac-toe/square.js
+++ src/components/tic-tac-toe/square.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import './game.css';
 
-class Square extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render () {
-        return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
-            </button>
-        )
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 export default Square;
